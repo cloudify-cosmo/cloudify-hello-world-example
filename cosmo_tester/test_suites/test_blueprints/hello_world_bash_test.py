a50dc0a6f10f0e424335c0cc2828dd3bc7a9bd10
Dan Kilman <dankilman@gmail.com>
2014-06-30 18:04:52 +0300

    CFY-861 refactor hello world test to be used by the two concurrent deployments test

--- cosmo_tester/test_suites/test_blueprints/hello_world_bash_test.py
+++ cosmo_tester/test_suites/test_blueprints/hello_world_bash_test.py
@@ -24,6 +24,7 @@ from cosmo_tester.framework.util import YamlPatcher
 from cosmo_tester.framework.testenv import TestCase
 from cosmo_tester.framework.util import get_yaml_as_dict
 from cosmo_tester.framework.openstack_api import openstack_clients
+from cosmo_tester.framework.git_helper import clone
 
 
 class HelloWorldBashTest(TestCase):
@@ -31,105 +32,80 @@ class HelloWorldBashTest(TestCase):
     CLOUDIFY_EXAMPLES_URL = "https://github.com/cloudify-cosmo/" \
                             "cloudify-examples.git"
 
-    host_name = 'bash-web-server'
-    security_groups = ['webserver_security_group']
-    virtual_ip_node_id = 'virtual_ip'
-    server_node_id = 'vm'
-    security_group_node_id = 'security_group'
-
-    repo_dir = None
-
-    def setUp(self):
-        super(HelloWorldBashTest, self).setUp()
-        from cosmo_tester.framework.git_helper import clone
-        self.repo_dir = clone(self.CLOUDIFY_EXAMPLES_URL, self.workdir)
-
     def test_hello_world_bash(self):
-
+        self.repo_dir = clone(self.CLOUDIFY_EXAMPLES_URL, self.workdir)
         self.blueprint_path = self.repo_dir / 'hello-world'
         self.blueprint_yaml = self.blueprint_path / 'blueprint.yaml'
+        self.modify_yaml(env=self.env,
+                         yaml_file=self.blueprint_yaml,
+                         host_name='bash-web-server',
+                         security_groups=['webserver_security_group'])
 
-        blueprint = get_yaml_as_dict(self.blueprint_yaml)
+        self.upload_deploy_and_execute_install(fetch_state=False)
 
-        self.modify_yaml(self.blueprint_yaml)
+        (floatingip_node,
+         security_group_node,
+         server_node) = self.get_instances(client=self.client,
+                                           deployment_id=self.test_id)
 
-        # Upload --> Create Deployment --> Execute Install
-        before, after = self.upload_deploy_and_execute_install()
-
-        manager_state_delta = self.get_manager_state_delta(before, after)
-        nodes_state = manager_state_delta['node_state'].values()[0]
-
-        server_node = None
-        security_group_node = None
-        floatingip_node = None
-        for key, value in nodes_state.items():
-            if key.startswith(self.virtual_ip_node_id):
-                floatingip_node = value
-            if key.startswith(self.security_group_node_id):
-                security_group_node = value
-            if key.startswith(self.server_node_id):
-                server_node = value
-
-        props_key = 'runtime_properties'
-        webserver_port = blueprint['blueprint']['nodes'][3]['properties'][
-            'port']
-        web_server_page_response = \
-            requests.get('http://{0}:{1}'.format(
-                floatingip_node[props_key]['floating_ip_address'],
-                webserver_port))
-
-        self.assertEqual(200, web_server_page_response.status_code)
+        self.verify_webserver_running(blueprint_yaml=self.blueprint_yaml,
+                                      floatingip_node=floatingip_node)
 
         nova, neutron = openstack_clients(self.env.cloudify_config)
 
-        self.logger.info("Retrieving agent server : {0}"
-                         .format(nova.servers.get(server_node[props_key][
-                             'openstack_server_id'])))
-        self.logger.info("Retrieving agent floating ip : {0}"
-                         .format(neutron.show_floatingip(floatingip_node[
-                             props_key]['external_id'])))
-        self.logger.info("Retrieving agent security group : {0}"
-                         .format(neutron.show_security_group(
-                             security_group_node[props_key][
-                                 'external_id'])))
+        server_id = server_node.runtime_properties['openstack_server_id']
+        floating_ip_id = floatingip_node.runtime_properties['external_id']
+        sg_id = security_group_node.runtime_properties['external_id']
+
+        nova_server = nova.servers.get(server_id)
+        neutron_floating_ip = neutron.show_floatingip(floating_ip_id)
+        neutron_sg = neutron.show_security_group(sg_id)
+        self.logger.info("Agent server : {0}".format(nova_server))
+        self.logger.info("Floating ip : {0}".format(neutron_floating_ip))
+        self.logger.info("Agent security group : {0}".format(neutron_sg))
 
         self.execute_uninstall()
-
         # No components should exist after uninstall
+        self.assertRaises(NotFound, nova.servers.get, server_id)
+        self.assertRaises(NeutronException, neutron.show_security_group, sg_id)
+        self.assertRaises(NeutronException, neutron.show_floatingip,
+                          floating_ip_id)
 
-        try:
-            server = nova.servers.get(server_node[props_key][
-                'openstack_server_id'])
-            self.fail("Expected agent machine to be terminated. but found : "
-                      "{0}".format(server))
-        except NotFound as e:
-            self.logger.info(e)
-            pass
+    @staticmethod
+    def verify_webserver_running(blueprint_yaml, floatingip_node):
+        blueprint = get_yaml_as_dict(blueprint_yaml)
+        webserver_props = blueprint['blueprint']['nodes'][3]['properties']
+        server_port = webserver_props['port']
+        server_ip = floatingip_node.runtime_properties['floating_ip_address']
+        server_response = requests.get('http://{0}:{1}'.format(server_ip,
+                                                               server_port))
+        if server_response.status_code != 200:
+            raise AssertionError('Unexpected status code: {}'
+                                 .format(server_response.status_code))
 
-        try:
-            floatingip = neutron.show_floatingip(floatingip_node[
-                props_key]['external_id'])
-            self.fail("Expected agent floating ip to be terminated. "
-                      "but found : {0}".format(floatingip))
-        except NeutronException as e:
-            self.logger.info(e)
-            pass
+    @staticmethod
+    def get_instances(client, deployment_id):
+        server_node = None
+        security_group_node = None
+        floatingip_node = None
+        instances = client.node_instances.list(
+            deployment_id=deployment_id)
+        for instance in instances:
+            if instance.node_id == 'virtual_ip':
+                floatingip_node = instance
+            if instance.node_id == 'security_group':
+                security_group_node = instance
+            if instance.node_id == 'vm':
+                server_node = instance
+        return floatingip_node, security_group_node, server_node
 
-        try:
-            security_group = neutron.show_security_group(security_group_node[
-                props_key]['external_id'])
-            self.fail("Expected webserver security group ip to be terminated. "
-                      "but found : {0}".format(security_group))
-        except NeutronException as e:
-            self.logger.info(e)
-            pass
-
-    def modify_yaml(self, yaml_file):
+    @staticmethod
+    def modify_yaml(env, yaml_file, host_name, security_groups):
         with YamlPatcher(yaml_file) as patch:
             vm_properties_path = 'blueprint.nodes[2].properties'
             patch.merge_obj('{0}.server'.format(vm_properties_path), {
-                'name': self.host_name,
-                'image_name': self.env.ubuntu_image_name,
-                'flavor_name': self.env.flavor_name,
-                'security_groups': self.security_groups,
+                'name': host_name,
+                'image_name': env.ubuntu_image_name,
+                'flavor_name': env.flavor_name,
+                'security_groups': security_groups,
             })
