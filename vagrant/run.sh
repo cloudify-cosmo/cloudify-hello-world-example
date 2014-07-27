f1a48cb1ab7a4b8bd1b9a7a7de8a9876b8613487
Dan Kilman <dankilman@gmail.com>
2014-07-27 17:56:13 +0300

    CFY-959 more integration

---
+++ vagrant/run.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -e
+
+create_virtualenv_if_needed_and_source()
+{
+    if [[ ! -d system_tests_controller_venv ]]; then
+        virtualenv system_tests_env
+        source system_tests_controller_venv/bin/activate
+        pip install pip --upgrade
+        pip install -r requirements.txt
+    else
+        source system_tests_controller_venv/bin/activate
+    fi
+}
+
+run_system_tests()
+{
+    ./controller.py
+}
+
+main()
+{
+    create_virtualenv_if_needed_and_source
+    run_system_tests
+}
+
+main
