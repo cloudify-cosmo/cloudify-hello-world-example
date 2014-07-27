d2b415929e98ffaeaa0e44dced43f09772c50c95
Dan Kilman <dankilman@gmail.com>
2014-07-27 17:28:27 +0300

    CFY-959 fix vars access

--- vagrant/quickbuild.groovy
+++ vagrant/quickbuild.groovy
@@ -1,2 +1,2 @@
 new File('temp.file').write(vars.getValue('system_tests_hp_username'))
-vars.setValue('system_tests_suites_path', 'suites.json')
+vars.get('system_tests_suites_path').setValue('suites.json')
