17d94d1d0c233d49f9932bdd5265b96fef2dd37a
Dan Kilman <dankilman@gmail.com>
2014-07-27 18:00:45 +0300

    CFY-959 bug fix

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -3,7 +3,7 @@
 create_virtualenv_if_needed_and_source()
 {
     if [[ ! -d system_tests_controller_venv ]]; then
-        virtualenv system_tests_env
+        virtualenv system_tests_controller_venv
         source system_tests_controller_venv/bin/activate
         pip install pip --upgrade
         pip install -r requirements.txt
