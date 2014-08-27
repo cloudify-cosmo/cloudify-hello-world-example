dae5db77330ada903d2e28af1cf153ee4f3536e1
Dan Kilman <dankilman@gmail.com>
2014-08-27 13:04:50 +0300

    add output to suites config creation

--- suites/helpers/suites_builder.py
+++ suites/helpers/suites_builder.py
@@ -5,14 +5,41 @@ import sys
 import os
 import json
 import tempfile
+import logging
+
+
+logger = logging.getLogger('suites_builder')
+logger.setLevel(logging.INFO)
+
 
 def build_suites_json(all_suites_json_path):
-    tests_suites = os.environ['SYSTEM_TESTS_SUITES'].split(',')
-    custom_suite = os.environ['SYSTEM_TESTS_CUSTOM_SUITE'] == 'yes'
-    custom_suite_name = os.environ['SYSTEM_TESTS_CUSTOM_SUITE_NAME']
-    custom_tests_to_run = os.environ['SYSTEM_TESTS_CUSTOM_TESTS_TO_RUN']
-    custom_cloudify_config = os.environ['SYSTEM_TESTS_CUSTOM_CLOUDIFY_CONFIG']
-    custom_handler_module = os.environ['SYSTEM_TESTS_CUSTOM_HANDLER_MODULE']
+    env_system_tests_suites = os.environ['SYSTEM_TESTS_SUITES']
+    env_custom_suite = os.environ['SYSTEM_TESTS_CUSTOM_SUITE']
+    env_custom_suite_name = os.environ['SYSTEM_TESTS_CUSTOM_SUITE_NAME']
+    env_custom_tests_to_run = os.environ['SYSTEM_TESTS_CUSTOM_TESTS_TO_RUN']
+    env_custom_cloudify_config = os.environ['SYSTEM_TESTS_CUSTOM_CLOUDIFY_CONFIG']
+    env_custom_handler_module = os.environ['SYSTEM_TESTS_CUSTOM_HANDLER_MODULE']
+
+    logger.info('Creating suites json configuration:\n'
+                '\tSYSTEM_TESTS_SUITES={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_SUITE={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_SUITE_NAME={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_TESTS_TO_RUN={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_CLOUDIFY_CONFIG={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_HANDLER_MODULE={}'
+                .format(env_system_tests_suites,
+                        env_custom_suite,
+                        env_custom_suite_name,
+                        env_custom_tests_to_run,
+                        env_custom_cloudify_config,
+                        env_custom_handler_module))
+
+    tests_suites = env_system_tests_suites.split(',')
+    custom_suite = env_custom_suite == 'yes'
+    custom_suite_name = env_custom_suite_name
+    custom_tests_to_run = env_custom_tests_to_run
+    custom_cloudify_config = env_custom_cloudify_config
+    custom_handler_module = env_custom_handler_module
 
     suites_json_path = tempfile.mktemp(prefix='suites-', suffix='.json')
 
