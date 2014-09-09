3c24033b08c091ce12e3cea3e8b70e485216a19e
adamlavie <adaml@gigaspaces.com>
2014-09-09 14:36:09 +0300

    fix update_config.py to handle specific cloud properties

--- suites/helpers/update_config.py
+++ suites/helpers/update_config.py
@@ -6,13 +6,7 @@ import os
 
 from cosmo_tester.framework.util import YamlPatcher
 
-properties = {
-    'AWS_ACCESS_ID'         : 'connection.access_id',
-    'AWS_SECRET_KEY'        : 'connection.secret_key',
-    'KEYSTONE_PASSWORD'     : 'keystone.password',
-    'KEYSTONE_USERNAME'     : 'keystone.username',
-    'KEYSTONE_TENANT'       : 'keystone.tenant_name',
-    'KEYSTONE_AUTH_URL'     : 'keystone.auth_url',
+shared_properties = {
     'RESOURCE_PREFIX'       : 'cloudify.resources_prefix',
     'COMPONENTS_PACKAGE_URL': 'cloudify.server.packages.components_package_url',
     'CORE_PACKAGE_URL'      : 'cloudify.server.packages.core_package_url',
@@ -22,8 +16,39 @@ properties = {
     'WINDOWS_PACKAGE_URL'   : 'cloudify.agents.packages.windows_agent_url',
 }
 
-with YamlPatcher(sys.argv[1]) as patch:
-    for env_var, prop_path in properties.items():
-        value = os.environ.get(env_var)
-        if value:
-            patch.set_value(prop_path, value)
+ec2_properties = {
+    'AWS_ACCESS_ID'         : 'connection.access_id',
+    'AWS_SECRET_KEY'        : 'connection.secret_key',
+}
+
+openstack_properties = {
+    'KEYSTONE_PASSWORD'     : 'keystone.password',
+    'KEYSTONE_USERNAME'     : 'keystone.username',
+    'KEYSTONE_TENANT'       : 'keystone.tenant_name',
+    'KEYSTONE_AUTH_URL'     : 'keystone.auth_url',
+}
+
+def main():
+    yaml_path = sys.argv[1]
+    handler = os.environ.get('CLOUDIFY_TEST_HANDLER_MODULE')
+    if handler in [
+        'cosmo_tester.framework.handlers.openstack',
+        'cosmo_tester.framework.handlers.simple_on_openstack']:
+        cloud_properties = openstack_properties
+    elif handler == 'cosmo_tester.framework.handlers.ec2':
+        cloud_properties = ec2_properties
+    else:
+        raise RuntimeError('Unsupported handler: {}'.format(handler))
+
+    properties = {}
+    properties.update(shared_properties)
+    properties.update(cloud_properties)
+
+    with YamlPatcher(yaml_path) as patch:
+        for env_var, prop_path in properties.items():
+            value = os.environ.get(env_var)
+            if value:
+                patch.set_value(prop_path, value)
+
+if __name__ == '__main__':
+    main()
