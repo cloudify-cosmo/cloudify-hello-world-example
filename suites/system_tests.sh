4f11d12c84c93b22b5a4b172152243cfb715b7ad
Ran Ziv <ran@gigaspaces.com>
2014-11-03 13:03:27 +0200

    CFY-1230 continued debugging

--- suites/system_tests.sh
+++ suites/system_tests.sh
@@ -32,6 +32,7 @@ setenv()
 	# WINDOWS_PACKAGE_URL=
 	# UI_PACKAGE_URL=
 
+    echo "### Using Providers1 = ${BOOTSTRAP_USING_PROVIDERS} ==="
     BOOTSTRAP_USING_PROVIDERS=${BOOTSTRAP_USING_PROVIDERS=false}
     echo "### Using Providers = ${BOOTSTRAP_USING_PROVIDERS} ==="
     CLOUDIFY_CONFIG_SUFFIX=$([ "$BOOTSTRAP_USING_PROVIDERS" == "false" ] && echo "json" || echo "yaml")
