1e3c85b380bbb5be9113f5f6ef87b14f9a3b7f25
adamlavie <adaml@gigaspaces.com>
2014-12-04 13:00:19 +0200

    CFY-1308 refactor tests

--- cosmo_tester/test_suites/test_docker_ubuntu_env/nodecellar_test.py
+++ cosmo_tester/test_suites/test_docker_ubuntu_env/nodecellar_test.py
@@ -13,25 +13,36 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+# Test docker bootstrap and installation of nodecellar on Ubuntu 14.04
 from cosmo_tester.framework.util import YamlPatcher
 from cosmo_tester.test_suites.test_blueprints import nodecellar_test
 
 
 class DockerNodeCellarTest(nodecellar_test.NodecellarAppTest):
 
+    UBUNTU_TRUSTY_IMAGE_MANE = 'Ubuntu Server 14.04.1 LTS (amd64 20140927)'
+    ' - Partner Image'
+    UNBUNTU_TRUSTY_IMAGE_ID = 'bec3cab5-4722-40b9-a78a-3489218e22fe'
+
     def test_docker_and_nodecellar(self):
         self._test_nodecellar_impl('openstack-blueprint.yaml',
-                                   self.env.ubuntu_image_id,
+                                   self.UBUNTU_TRUSTY_IMAGE_MANE,
                                    self.env.flavor_name)
 
-    def modify_blueprint(self):
+    def modify_blueprint(self, image_name, flavor_name):
         with YamlPatcher(self.blueprint_yaml) as patch:
             vm_props_path = 'node_types.vm_host.properties'
+            # Add required docker param. See CFY-816
             patch.merge_obj('{0}.cloudify_agent.default'
                             .format(vm_props_path), {
                                 'home_dir': '/home/ubuntu'
                             })
-            # use ubuntu trusty 14.04 as agent machine
-            # patch.merge_obj('{0}.server.default'.format(vm_props_path), {
-            #     'image': '8ca068c5-6fde-4701-bab8-322b3e7c8d81'
-            # })
+            vm_type_path = 'node_types.vm_host.properties'
+            patch.merge_obj('{0}.server.default'.format(vm_type_path), {
+                'image_name': image_name,
+                'flavor_name': flavor_name
+            })
+            # Use ubuntu trusty 14.04 as agent machine
+            patch.merge_obj('{0}.server.default'.format(vm_props_path), {
+                'image': self.UNBUNTU_TRUSTY_IMAGE_ID
+            })
