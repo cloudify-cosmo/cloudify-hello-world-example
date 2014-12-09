6ba1a5df148b13590e5a89cdd6c2f6e49b1a9370
adamlavie <adaml@gigaspaces.com>
2014-12-09 23:17:06 +0200

    fixed test

--- cosmo_tester/test_suites/test_docker_centos_env/nodecellar_test.py
+++ cosmo_tester/test_suites/test_docker_centos_env/nodecellar_test.py
@@ -20,14 +20,20 @@ from cosmo_tester.test_suites.test_blueprints import nodecellar_test
 class DockerNodeCellarTest(nodecellar_test.NodecellarAppTest):
 
     def test_docker_and_nodecellar(self):
-        self._test_nodecellar_impl('openstack-blueprint.yaml',
-                                   self.env.ubuntu_image_name,
-                                   self.env.flavor_name)
+        self._test_nodecellar_impl('openstack-blueprint.yaml')
 
     def modify_blueprint(self, image_name, flavor_name):
         with YamlPatcher(self.blueprint_yaml) as patch:
             vm_props_path = 'node_types.vm_host.properties'
             patch.merge_obj('{0}.cloudify_agent.default'
                             .format(vm_props_path), {
-                                'home_dir': '/home/ubuntu'
+                                'home_dir': '/home/centos'
                             })
+
+    def get_inputs(self):
+
+        return {
+            'image': self.env.ubuntu_image_id,
+            'flavor': self.env.small_flavor_id,
+            'agent_user': 'ubuntu'
+        }
