6a7d61a81fbcf1285db282c1d4b537116c199229
Dan Kilman <dankilman@gmail.com>
2015-01-01 15:55:44 +0200

    CFY-1850 update suites builder to accept suites decriptor for custom suites

--- suites/helpers/suites_builder.py
+++ suites/helpers/suites_builder.py
@@ -1,8 +1,4 @@
-#! /usr/bin/env python
-# flake8: NOQA
-
 import os
-import json
 import tempfile
 import logging
 
@@ -14,43 +10,30 @@ logger.setLevel(logging.INFO)
 
 def build_suites_yaml(all_suites_yaml_path):
     env_system_tests_suites = os.environ['SYSTEM_TESTS_SUITES']
-    env_custom_suite = os.environ['SYSTEM_TESTS_CUSTOM_SUITE']
-    env_custom_suite_name = os.environ['SYSTEM_TESTS_CUSTOM_SUITE_NAME']
-    env_custom_handler_configuration = os.environ['SYSTEM_TESTS_CUSTOM_HANDLER_CONFIGURATION']
-    env_custom_tests_to_run = os.environ['SYSTEM_TESTS_CUSTOM_TESTS_TO_RUN']
+    env_custom = os.environ['SYSTEM_TESTS_CUSTOM']
+    env_custom_descriptor = os.environ['SYSTEM_TESTS_CUSTOM_DESCRIPTOR']
 
-    logger.info('Creating suites json configuration:\n'
+    logger.info('Generating suites yaml:\n'
                 '\tSYSTEM_TESTS_SUITES={}\n'
-                '\tSYSTEM_TESTS_CUSTOM_SUITE={}\n'
-                '\tSYSTEM_TESTS_CUSTOM_SUITE_NAME={}\n'
-                '\tSYSTEM_TESTS_CUSTOM_TESTS_TO_RUN={}\n'
-                '\tSYSTEM_TESTS_CUSTOM_HANDLER_CONFIGURATION={}\n'
+                '\tSYSTEM_TESTS_CUSTOM={}\n'
+                '\tSYSTEM_TESTS_CUSTOM_DESCRIPTOR={}\n'
                 .format(env_system_tests_suites,
-                        env_custom_suite,
-                        env_custom_suite_name,
-                        env_custom_tests_to_run,
-                        env_custom_handler_configuration))
+                        env_custom,
+                        env_custom_descriptor))
 
-    tests_suites_names = [s.strip() for s in env_system_tests_suites.split(',')]
-    custom_suite = env_custom_suite == 'yes'
-    custom_suite_name = env_custom_suite_name
-    custom_tests_to_run = [s.strip() for s in env_custom_tests_to_run.split(',')]
-    custom_handler_configuration = env_custom_handler_configuration
+    tests_suites_names = [s.strip() for s
+                          in env_system_tests_suites.split(',')]
+    custom = env_custom == 'yes'
 
     with open(all_suites_yaml_path) as f:
         suites_yaml = yaml.load(f.read())
     suites_yaml_path = tempfile.mktemp(prefix='suites-', suffix='.json')
 
-    if custom_suite:
-        test_suites = {
-            custom_suite_name: {
-                'tests': custom_tests_to_run,
-                'handler_configuration': custom_handler_configuration
-            }
-        }
+    if custom:
+        test_suites = parse_custom_descriptor(env_custom_descriptor)
     else:
-        all_test_suites = suites_yaml['test_suites']
-        test_suites = {suite_name: suite for suite_name, suite in all_test_suites
+        test_suites = {suite_name: suite for suite_name, suite
+                       in suites_yaml['test_suites']
                        if suite_name in tests_suites_names}
 
     suites_yaml['test_suites'] = test_suites
@@ -58,3 +41,16 @@ def build_suites_yaml(all_suites_yaml_path):
         f.write(yaml.safe_dump(suites_yaml))
 
     return suites_yaml_path
+
+
+def parse_custom_descriptor(custom_descriptor):
+    result = {}
+    suite_descriptors = custom_descriptor.split('#')
+    for i, suite_descriptor in enumerate(suite_descriptors, start=1):
+        handler_configuration, tests = suite_descriptor.split('@')
+        tests = tests.split(' ')
+        result['{0}{1}'.format(handler_configuration, i)] = {
+            'handler_configuration': handler_configuration,
+            'tests': tests
+        }
+    return result
