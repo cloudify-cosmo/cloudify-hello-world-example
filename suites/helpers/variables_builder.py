72d83212f44ef5bf38e85794a09025408d87457c
Dan Kilman <dankilman@gmail.com>
2015-01-08 01:13:13 +0200

    CFY-1850 merge all parent configuration variables

--- suites/helpers/variables_builder.py
+++ suites/helpers/variables_builder.py
@@ -17,40 +17,57 @@ def main():
     variables_path = sys.argv[1]
     username = os.environ['QUICK_BUILD_USER']
     password = os.environ['QUICK_BUILD_PASSWORD']
+    auth = (username, password)
+    qb_url = os.environ['QUICK_BUILD_URL']
     build_id = os.environ['QUICK_BUILD_BUILD_ID']
     configuration_id = os.environ['QUICK_BUILD_CONFIGURATION_ID']
-    qb_url = os.environ['QUICK_BUILD_URL']
-    build_endpoint = '{0}/rest/builds/{1}'.format(qb_url, build_id)
-    configuration_endpoint = '{0}/rest/configurations/{1}'\
-                             .format(qb_url, configuration_id)
-    xml_build = requests.get(build_endpoint, auth=(username, password)).text
-    xml_configuration = requests.get(configuration_endpoint,
-                                     auth=(username, password)).text
-    build = xmltodict.parse(xml_build)
-    configuration = xmltodict.parse(xml_configuration)
 
-    configuration_variables = configuration[
-        'com.pmease.quickbuild.model.Configuration']['variables'][
-        'com.pmease.quickbuild.variable.Variable']
-    configuration_variables = {e['name']: e['valueProvider']['value']
-                               for e in configuration_variables}
+    top_level_configuration_id = 1
+    configuration_ids = [configuration_id]
+    while configuration_id != top_level_configuration_id:
+        configuration_id = _get_parent_configuration_id(
+            configuration_id, qb_url, auth)
+        configuration_ids.insert(0, configuration_id)
 
-    build_variables = build['com.pmease.quickbuild.model.Build'][
-        'variableValues']['entry']
-    build_variables = {e['string'][0]: e['string'][1]
-                       for e in build_variables}
-
-    variables = configuration_variables
-    variables.update(build_variables)
+    variables = {}
+    for configuration_id in configuration_ids:
+        variables.update(_read_configuration_variables(
+            configuration_id, qb_url, auth))
+    variables.update(_read_build_variables(build_id, qb_url, auth))
 
     yaml_dump = yaml.safe_dump(variables)
     with open(variables_path, 'w') as f:
         f.write(yaml_dump)
 
-    logger.info('{0}'.format(build))
-    logger.info('{0}'.format(configuration))
     logger.info('Extracted QuickBuild variables:\n{0}'
                 .format(yaml_dump))
+
+
+def _get_parent_configuration_id(configuration_id, qb_url, auth):
+    parent_endpoint = ('{0}/rest/configurations/{1}/parent'
+                       .format(qb_url, configuration_id))
+    return requests.get(parent_endpoint, auth=auth).text
+
+
+def _read_configuration_variables(configuration_id, qb_url, auth):
+    configuration_endpoint = ('{0}/rest/configurations/{1}'
+                              .format(qb_url, configuration_id))
+    xml_configuration = requests.get(configuration_endpoint, auth=auth).text
+    configuration = xmltodict.parse(xml_configuration)
+    configuration_variables = configuration[
+        'com.pmease.quickbuild.model.Configuration']['variables'][
+        'com.pmease.quickbuild.variable.Variable']
+    return {e['name']: e['valueProvider']['value']
+            for e in configuration_variables}
+
+
+def _read_build_variables(build_id, qb_url, auth):
+    build_endpoint = '{0}/rest/builds/{1}'.format(qb_url, build_id)
+    xml_build = requests.get(build_endpoint, auth=auth).text
+    build = xmltodict.parse(xml_build)
+    build_variables = build['com.pmease.quickbuild.model.Build'][
+        'variableValues']['entry']
+    return {e['string'][0]: e['string'][1] for e in build_variables}
 
 
 if __name__ == '__main__':
