15ee1557df253d10da60a6d0e6900c36bc31f0d3
Dan Kilman <dankilman@gmail.com>
2015-01-07 20:31:25 +0200

    CFY-1850 also read configuration variables

--- suites/helpers/variables_builder.py
+++ suites/helpers/variables_builder.py
@@ -18,18 +18,37 @@ def main():
     username = os.environ['QUICK_BUILD_USER']
     password = os.environ['QUICK_BUILD_PASSWORD']
     build_id = os.environ['QUICK_BUILD_BUILD_ID']
+    configuration_id = os.environ['QUICK_BUILD_CONFIGURATION_ID']
     qb_url = os.environ['QUICK_BUILD_URL']
-    endpoint = '{0}/rest/builds/{1}'.format(qb_url, build_id)
-    xml_build = requests.get(endpoint, auth=(username, password)).text
+    build_endpoint = '{0}/rest/builds/{1}'.format(qb_url, build_id)
+    configuration_endpoint = '{0}/rest/configurations/{1}'\
+                             .format(qb_url, configuration_id)
+    xml_build = requests.get(build_endpoint, auth=(username, password)).text
+    xml_configuration = requests.get(configuration_endpoint,
+                                     auto=(username, password)).text
     build = xmltodict.parse(xml_build)
-    logger.info('{0}'.format(build))
-    variables = build['com.pmease.quickbuild.model.Build']['variableValues'][
-        'entry']
-    variables = {e['string'][0]: e['string'][1]
-                 for e in variables}
+    configuration = xmltodict.parse(xml_configuration)
+
+    configuration_variables = configuration[
+        'com.pmease.quickbuild.model.Configuration']['variables'][
+        'com.pmease.quickbuild.variable.Variable']
+    configuration_variables = {e['name']: e['valueProvider']['value']
+                               for e in configuration_variables}
+
+    build_variables = build['com.pmease.quickbuild.model.Build'][
+        'variableValues']['entry']
+    build_variables = {e['string'][0]: e['string'][1]
+                       for e in build_variables}
+
+    variables = configuration_variables
+    variables.update(build_variables)
+
     yaml_dump = yaml.safe_dump(variables)
     with open(variables_path, 'w') as f:
         f.write(yaml_dump)
+
+    logger.info('{0}'.format(build))
+    logger.info('{0}'.format(configuration))
     logger.info('Extracted QuickBuild variables:\n{0}'
                 .format(yaml_dump))
 
