62bddd3fa92f4e3ff2a2b191ec43fd4def62d50e
Dan Kilman <dankilman@gmail.com>
2015-01-07 19:27:16 +0200

    CFY-1850 implement inputs builder for quickbuild

--- suites/helpers/inputs_builder.py
+++ suites/helpers/inputs_builder.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import tempfile
 
+import yaml
 import requests
 import xmltodict
 
@@ -10,6 +12,17 @@ def main():
     password = os.environ['QUICK_BUILD_PASSWORD']
     build_id = os.environ['QUICK_BUILD_BUILD_ID']
     qb_url = os.environ['QUICK_BUILD_URL']
+    endpoint = '{0}/rest/builds/{1}'.format(qb_url, build_id)
+    xml_build = requests.get(endpoint, auth=(username, password)).text
+    build = xmltodict.parse(xml_build)
+    variables = build['com.pmease.quickbuild.model.Build']['variableValues'][
+        'entry']
+    variables = {e['string'][0]: e['string'][1]
+                 for e in variables}
+    variables_path = tempfile.mktemp()
+    with open(variables_path, 'w') as f:
+        f.write(yaml.safe_dump(variables))
+    sys.stdout.write(variables_path)
 
 if __name__ == '__main__':
     main()
