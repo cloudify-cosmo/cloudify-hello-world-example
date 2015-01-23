05fcce76e919c7ff4fc28770c25beb2e64462dcf
Dan Kilman <dankilman@gmail.com>
2015-01-23 02:37:34 +0200

    fix test

--- cosmo_tester/framework/util.py
+++ cosmo_tester/framework/util.py
@@ -126,7 +126,7 @@ def check_port(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((ip, port))
     try:
-        sock.shutdown()
+        sock.shutdown(socket.SHUT_RDWR)
         sock.close()
     except socket.error:
         pass
