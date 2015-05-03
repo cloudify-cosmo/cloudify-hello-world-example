13bc8ef7737ee9a5495bf642eef9531b8534dd2a
Dan Kilman <dankilman@gmail.com>
2015-05-03 21:48:27 +0300

    more unicode grieve

--- suites/helpers/__init__.py
+++ suites/helpers/__init__.py
@@ -1,7 +1,15 @@
 import sys
 
 
+def _write(stream, s):
+    try:
+        s = unicode(s)
+    except UnicodeDecodeError:
+        s = str(s).decode(encoding, 'replace')
+    stream.write(s)
+
+
 def sh_bake(command):
     return command.bake(
-        _out=lambda line: sys.stdout.write(line.decode('utf-8')),
-        _err=lambda line: sys.stderr.write(line.decode('utf-8')))
+        _out=lambda line: _write(sys.stdout, line),
+        _err=lambda line: _write(sys.stderr, line))
