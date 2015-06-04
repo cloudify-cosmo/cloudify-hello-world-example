c3ef5452aa7e8c67d4a7a59dc1733b4876a98fa1
opencm <limor@gigaspaces.com>
2015-06-04 16:47:58 +0300

    Bump version to 3.3m1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-system-tests',
-    version='3.2',
+    version='3.3a1',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['cosmo_tester'],
@@ -32,7 +32,7 @@ setup(
         'path.py==5.1',
         'nose',
         'retrying==1.2.2',
-        'cloudify==3.2',
+        'cloudify==3.3a1',
         'elasticsearch',
         'Jinja2==2.7.2',
         'influxdb==0.1.13',
