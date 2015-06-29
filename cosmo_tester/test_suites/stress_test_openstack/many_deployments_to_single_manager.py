325afc29dd1153d4dc8d77bba7083718486749b9
gilzellner <gil.zellner@gmail.com>
2015-06-29 15:13:37 +0300

    CFY-2812-Stress-test-to-see-how-many-deployments-can-a-single-manager-handle

--- cosmo_tester/test_suites/stress_test_openstack/many_deployments_to_single_manager.py
+++ cosmo_tester/test_suites/stress_test_openstack/many_deployments_to_single_manager.py
@@ -31,6 +31,13 @@ def num(s):
 
 class ManyDeploymentsTest(MonitoringTestCase):
 
+    def wait_until_all_deployment_executions_end(self, deployment_id):
+        while len([execution for execution in self.client.executions.list(
+                deployment_id=deployment_id)
+                if execution["status"] in ["started", "pending"]]) > 0:
+                        sleep(1)
+        return
+
     def many_deployments_stress_test(self):
         self._run()
 
@@ -72,14 +79,18 @@ class ManyDeploymentsTest(MonitoringTestCase):
                          .format(self.env.management_ip))
         return int(
             str(fabric.api.run(
-                'sudo docker exec cfy df -k | grep /dev/vdc1 | awk \'{print $2}\'')).replace("sudo: unable to resolve host cloudify-manager-server",""))
+                'sudo docker exec cfy df -k | grep /dev/vdc1 | '
+                'awk \'{print $2}\'')).replace(
+                "sudo: unable to resolve host cloudify-manager-server", ""))
 
     def get_manager_disk_available(self):
         self.logger.info('get_manager_disk_available with ip {0}'
                          .format(self.env.management_ip))
         return int(
             str(fabric.api.run(
-                'sudo docker exec cfy df -k | grep /dev/vdc1 | awk \'{print $4}\'')).replace("sudo: unable to resolve host cloudify-manager-server",""))
+                'sudo docker exec cfy df -k | grep /dev/vdc1 | '
+                'awk \'{print $4}\'')).replace(
+                "sudo: unable to resolve host cloudify-manager-server", ""))
 
     def get_number_of_total_active_nodes(self):
         return len((self.client.nodes.list(
@@ -92,8 +103,12 @@ class ManyDeploymentsTest(MonitoringTestCase):
             _include=["deploy_number_of_instances",
                       "deployment_id"])))
 
+    def _end_test(self, report):
+        self.logger.info(json.dumps(report, indent=2))
+        return report
+
     def _run(self):
-        number_of_deployments = 117
+        number_of_deployments = 1
         self.init_fabric()
         blueprint_path = self.copy_blueprint('mocks')
         self.blueprint_yaml = blueprint_path / 'single-node-blueprint.yaml'
@@ -114,77 +129,77 @@ class ManyDeploymentsTest(MonitoringTestCase):
                            "manager_disk_space_total":
                                manager_disk_space_total}
         deployments_dict = {0: deployment_dict}
-        for i in range(1, number_of_deployments+1):
-            start_time = time.time()
-            self.create_deployment(blueprint_id=self.test_id,
-                                   deployment_id=self.test_id+str(i),
-                                   inputs='')
-            while [] != \
-                    [execution for execution in self.client.executions.list(
-                        deployment_id=self.test_id+str(i))
-                     if execution["status"] in ["started", "pending"]]:
-                sleep(1)
-            end_create_deployment_time = time.time()
-            start_install_time = time.time()
-            self.client.executions.start(
-                deployment_id=self.test_id+str(i),
-                workflow_id="install")
-            while [] != \
-                    [execution for execution in self.client.executions.list(
-                        deployment_id=self.test_id+str(i))
-                     if execution["status"] in ["started", "pending"]]:
-                sleep(1)
-            end_execute_install_time = time.time()
-            self.logger.debug(
-                "time to create deployment number {0} : {1}".format(
-                    i, end_create_deployment_time - start_time))
-            self.logger.debug(
-                "time to execute install number {0} : {1}".format(
-                    i, end_execute_install_time - start_install_time))
-            manager_disk_space_available = self.get_manager_disk_available()
-            manager_memory_available = self.get_manager_memory_available()
-            number_of_active_nodes = self.get_number_of_total_active_nodes()
-            number_of_my_active_nodes = \
-                self.get_number_of_active_nodes_per_deployment(
+        try:
+            while True:
+                start_time = time.time()
+                self.create_deployment(blueprint_id=self.test_id,
+                                       deployment_id=self.test_id+str(
+                                           number_of_deployments),
+                                       inputs='')
+                self.wait_until_all_deployment_executions_end(
+                    deployment_id=self.test_id+str(number_of_deployments))
+                end_create_deployment_time = time.time()
+                start_install_time = time.time()
+                self.client.executions.start(
+                    deployment_id=self.test_id+str(number_of_deployments),
+                    workflow_id="install")
+                self.wait_until_all_deployment_executions_end(
+                    deployment_id=self.test_id+str(number_of_deployments))
+                end_execute_install_time = time.time()
+                self.logger.debug(
+                    "time to create deployment number {0} : {1}".format(
+                        number_of_deployments,
+                        end_create_deployment_time - start_time))
+                self.logger.debug(
+                    "time to execute install number {0} : {1}".format(
+                        number_of_deployments,
+                        end_execute_install_time - start_install_time))
+                manager_disk_space_available = \
+                    self.get_manager_disk_available()
+                manager_memory_available = self.get_manager_memory_available()
+                number_of_active_nodes = \
+                    self.get_number_of_total_active_nodes()
+                number_of_my_active_nodes = \
+                    self.get_number_of_active_nodes_per_deployment(
+                        deployment_id=self.test_id+str(number_of_deployments))
+                deployment_dict = {"deployment_number": number_of_deployments,
+                                   "number_of_my_active_nodes":
+                                       number_of_my_active_nodes,
+                                   "nodes_active": number_of_active_nodes,
+                                   "time_to_create_deployment": limit_dpoints(
+                                       end_create_deployment_time -
+                                       start_time),
+                                   "time_to_install":
+                                       limit_dpoints(
+                                           end_execute_install_time -
+                                           start_install_time),
+                                   "manager_memory_available":
+                                       manager_memory_available,
+                                   "manager_memory_total":
+                                       manager_memory_total,
+                                   "manager_disk_space_available":
+                                       manager_disk_space_available,
+                                   "manager_disk_space_total":
+                                       manager_disk_space_total,
+                                   "memory_change_in_deployment":
+                                       prev_manager_memory_available -
+                                       manager_memory_available,
+                                   "manager_cpu_usage":
+                                       self.get_manager_cpu_usage(),
+                                   "disk_change_in_deployment":
+                                       prev_space_available -
+                                       manager_disk_space_available}
+                prev_space_available = manager_disk_space_available
+                prev_manager_memory_available = manager_memory_available
+                self.logger.debug(deployment_dict)
+                deployments_dict.update(
+                    {number_of_deployments: deployment_dict})
+                number_of_deployments += 1
+                self.logger.debug(deployments_dict)
+            for i in range(1, number_of_deployments+1):
+                self.wait_until_all_deployment_executions_end(
                     deployment_id=self.test_id+str(i))
-            deployment_dict = {"deployment_number": i,
-                               "number_of_my_active_nodes":
-                                   number_of_my_active_nodes,
-                               "nodes_active": number_of_active_nodes,
-                               "time_to_create_deployment": limit_dpoints(
-                                   end_create_deployment_time - start_time),
-                               "time_to_install":
-                                   limit_dpoints(
-                                       end_execute_install_time -
-                                       start_install_time),
-                               "manager_memory_available":
-                                   manager_memory_available,
-                               "manager_memory_total":
-                                   manager_memory_total,
-                               "manager_disk_space_available":
-                                   manager_disk_space_available,
-                               "manager_disk_space_total":
-                                   manager_disk_space_total,
-                               "memory_change_in_deployment":
-                                   prev_manager_memory_available -
-                                   manager_memory_available,
-                               "manager_cpu_usage":
-                                   self.get_manager_cpu_usage(),
-                               "disk_change_in_deployment":
-                                   prev_space_available -
-                                   manager_disk_space_available}
-            prev_space_available = manager_disk_space_available
-            prev_manager_memory_available = manager_memory_available
-            self.logger.debug(deployment_dict)
-            deployments_dict.update({i: deployment_dict})
-        for i in range(1, number_of_deployments+1):
-            executions_list = self.client.executions.list(
-                deployment_id=self.test_id+str(i))
-            while executions_list != []:
-                executions_list = self.client.executions.list(
-                    deployment_id=self.test_id+str(i))
-                executions_list = \
-                    [execution for execution in executions_list if
-                     execution["status"] in ["started", "pending"]]
-                sleep(1)
-        self.logger.info(json.dumps(deployments_dict, indent=2))
+            self._end_test(deployments_dict)
+        except Exception as e:
+            print e
+            self._end_test(deployments_dict)
