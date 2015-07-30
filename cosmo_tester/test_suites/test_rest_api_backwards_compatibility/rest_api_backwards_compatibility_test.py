7c81f46bccedcd62c234413ffa81270bca612b6c
Noa Kuperberg <noakup@gmail.com>
2015-07-30 18:19:24 +0300

    CFY-2994 - new script templates added

--- cosmo_tester/test_suites/test_rest_api_backwards_compatibility/rest_api_backwards_compatibility_test.py
+++ cosmo_tester/test_suites/test_rest_api_backwards_compatibility/rest_api_backwards_compatibility_test.py
@@ -18,13 +18,19 @@ import json
 import os
 import shutil
 import subprocess
+import pkg_resources
+import cosmo_tester
+from jinja2.environment import Template
 
 from cosmo_tester.framework.testenv import TestCase
 
 
+SHELL_SCRIPT_TEMPLATE = 'run_old_rest_client_in_shell.template'
+PYTHON_SCRIPT_TEMPLATE = 'use_old_rest_client.template'
 SHELL_SCRIPT_NAME = 'test_old_client.sh'
 PYTHON_SCRIPT_NAME = 'test_old_client.py'
-VIRTUAL_ENV = 'cfy_32_cli_env'
+VENV_NAME = 'cfy_32_cli_env'
+VENV_FOLDER = '/tmp'
 CFY_CLIENT_VERSION = '3.2'
 
 
@@ -32,15 +38,13 @@ class RestApiBackwardsCompatibilityTest(TestCase):
 
     def setUp(self):
         super(RestApiBackwardsCompatibilityTest, self).setUp()
-        self._create_python_script(PYTHON_SCRIPT_NAME, self.env.management_ip)
-        self._create_shell_script(SHELL_SCRIPT_NAME,
-                                  PYTHON_SCRIPT_NAME,
-                                  VIRTUAL_ENV,
-                                  CFY_CLIENT_VERSION)
+        self._render_python_script()
+        self._render_shell_script()
 
     def tearDown(self):
-        if os.path.exists(VIRTUAL_ENV):
-            shutil.rmtree(VIRTUAL_ENV)
+        venv_path = os.path.join(VENV_FOLDER, VENV_NAME)
+        if os.path.exists(venv_path):
+            shutil.rmtree(venv_path)
 
         if os.path.exists(PYTHON_SCRIPT_NAME):
             os.remove(PYTHON_SCRIPT_NAME)
@@ -50,44 +54,33 @@ class RestApiBackwardsCompatibilityTest(TestCase):
 
         super(RestApiBackwardsCompatibilityTest, self).tearDown()
 
-    def _create_python_script(self, python_script_name, cfy_manager_ip):
-        with open(python_script_name, 'w') as f:
-            f.write('import json\n')
-            f.write('from cloudify_rest_client.client import CloudifyClient\n')
-            f.write('def run_test():\n')
-            f.write('    output = {}\n')
-            f.write('    try:\n')
-            f.write('        rest_client = CloudifyClient(host="{0}")\n'
-                    .format(cfy_manager_ip))
-            f.write('        rest_client_url = rest_client._client.url\n')
-            f.write('        expected_rest_client_url = "http://{0}:80"\n'
-                    .format(cfy_manager_ip))
-            f.write('        if rest_client_url==expected_rest_client_url:\n')
-            f.write('            status = rest_client.manager.get_status()\n')
-            f.write('            output["exit_code"] = 0\n')
-            f.write('            output["details"] = status\n')
-            f.write('        else:\n')
-            f.write('            output["exit_code"] = 1\n')
-            f.write('            output["details"] = "rest client url is {0} '
-                    'instead of {1}".format(rest_client_url, '
-                    'expected_rest_client_url)\n')
-            f.write('    except Exception as e:\n')
-            f.write('        output["exit_code"] = 1\n')
-            f.write('        output["details"] = e.message\n')
-            f.write('    return output\n')
-            f.write('out = run_test()\n')
-            f.write('print json.dumps(out)')
+    def _render_python_script(self):
+        python_script_template = pkg_resources.resource_string(
+            cosmo_tester.__name__,
+            'resources/scripts/{0}'.format(PYTHON_SCRIPT_TEMPLATE)
+        )
+        rendered_python_script = Template(python_script_template). \
+            render(cfy_manager_ip=self.env.management_ip)
+        with open(PYTHON_SCRIPT_NAME, 'w') as f:
+            f.write(rendered_python_script)
 
-    def _create_shell_script(self, shell_script_name, python_script_name,
-                             env_name, cfy_client_version):
-        with open(shell_script_name, 'w') as f:
-            f.write('virtualenv {0} >/dev/null\n'.format(env_name))
-            f.write('source {0}/bin/activate >/dev/null\n'.format(env_name))
-            f.write('pip install cloudify=={0} >/dev/null\n'
-                    .format(cfy_client_version))
-            f.write('python {0}\n'.format(python_script_name))
-        permissions = os.stat(shell_script_name)
-        os.chmod(shell_script_name, permissions.st_mode | 0111)
+    def _render_shell_script(self):
+        shell_script_template = pkg_resources.resource_string(
+            cosmo_tester.__name__,
+            'resources/scripts/{0}'.format(SHELL_SCRIPT_TEMPLATE)
+        )
+        template_values = {'venv_name': VENV_NAME,
+                           'venv_folder': VENV_FOLDER,
+                           'client_version': CFY_CLIENT_VERSION,
+                           'python_script_name': PYTHON_SCRIPT_NAME}
+        rendered_shell_script = Template(shell_script_template).\
+            render(template_values)
+        with open(SHELL_SCRIPT_NAME, 'w') as f:
+            f.write(rendered_shell_script)
+
+        # set permission to execute file
+        permissions = os.stat(SHELL_SCRIPT_NAME)
+        os.chmod(SHELL_SCRIPT_NAME, permissions.st_mode | 0111)
 
     def test_old_client_vs_new_server(self):
         output = subprocess.check_output(
