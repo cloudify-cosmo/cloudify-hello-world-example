edb09f8c943448cdd34fe28403a392030c10394e
Idan Moyal <idan@gigaspaces.com>
2015-08-04 11:53:22 +0300

    CFY-3285 added a singlehost blueprint

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,19 +15,36 @@ ctx logger info "Starting SimpleHTTPServer"
 nohup python -m SimpleHTTPServer ${port} > /dev/null 2>&1 &
 echo $! > ${PID_FILE}
 
-ctx logger info "Waiting for server to launch"
+ctx logger info "Waiting for server to launch on port ${port}"
+url="http://localhost:${port}"
+
+server_is_up() {
+	if which wget >/dev/null; then
+		if wget $url >/dev/null; then
+			return 0
+		fi
+	elif which curl >/dev/null; then
+		if curl $url >/dev/null; then
+			return 0
+		fi
+	else
+		ctx logger error "Both curl, wget were not found in path"
+		exit 1
+	fi
+	return 1
+}
 
 STARTED=false
 for i in $(seq 1 15)
 do
-	if wget http://localhost:${port} 2>/dev/null ; then
+	if server_is_up; then
 		ctx logger info "Server is up."
 		STARTED=true
     	break
 	else
 		ctx logger info "Server not up. waiting 1 second."
 		sleep 1
-	fi	
+	fi
 done
 if [ ${STARTED} = false ]; then
 	ctx logger error "Failed starting web server in 15 seconds."
