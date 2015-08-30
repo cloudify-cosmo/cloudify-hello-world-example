2ef7d800b827980e3e8557ce5c65ed76fa1861a6
Idan Moyal <idan@gigaspaces.com>
2015-08-30 14:06:54 +0300

    add cleanup for removing created keypair in test

--- cosmo_tester/test_suites/test_blueprints/test_existing_vm.py
+++ cosmo_tester/test_suites/test_blueprints/test_existing_vm.py
@@ -82,13 +82,19 @@ class ExistingVMTest(TestCase):
         self.logger.info('Uninstalling deployment...')
         self.execute_uninstall()
 
+    def delete_keypair(self, nova_client, keypair):
+        self.logger.info('Deleting keypair: {0}'.format(keypair))
+        nova_client.keypairs.delete(keypair)
+
     def create_keypair_and_copy_to_manager(self,
                                            nova_client,
                                            remote_key_path,
                                            key_name):
         key_file = path(self.workdir) / '{}.pem'.format(key_name)
         keypair = nova_client.keypairs.create(key_name)
-        key_file.write_text(keypair.private_key)
+        self.addCleanup(self.delete_keypair, nova_client, keypair)
+
+        key_file.write_text(self.keypair.private_key)
         key_file.chmod(0600)
 
         management_key_path = get_actual_keypath(self.env,
