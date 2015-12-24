5293eb6b63363ac432f4471d67bee2ddc5cbb5a5
opencm <limor@gigaspaces.com>
2015-12-24 22:56:28 +0200

    Bump version to 3.4m1

--- cosmo_tester/resources/plugins/mock-rest-plugin/setup.py
+++ cosmo_tester/resources/plugins/mock-rest-plugin/setup.py
@@ -35,7 +35,7 @@ class InstallCommand(install):
 
 setup(
     name='mock-rest-plugin',
-    version='3.3',
+    version='1.3.1',
     packages=['mock_rest_plugin'],
     cmdclass={
         'install': InstallCommand,
