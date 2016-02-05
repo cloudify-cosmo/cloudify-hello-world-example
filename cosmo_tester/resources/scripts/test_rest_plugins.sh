09d29f9cbded68467f1feb61eeedad89145362b7
Dan Kilman <dank@gigaspaces.com>
2016-02-05 09:51:38 +0200

    CFY-4029 fix test

--- cosmo_tester/resources/scripts/test_rest_plugins.sh
+++ cosmo_tester/resources/scripts/test_rest_plugins.sh
@@ -2,4 +2,4 @@
 set -e
 source /opt/manager/env/bin/activate
 python -c "import plugin.tasks; print 'imported_plugin_tasks'"
-python -c "import mock_wagon_plugin; print mock_wagon_plugin.mock_attribute"
+python -c "import mock_rest_plugin; print mock_rest_plugin.mock_attribute"
