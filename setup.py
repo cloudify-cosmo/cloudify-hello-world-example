268336c1132a93c0bc5a67780f4b0d65e4512592
opencm <limor@gigaspaces.com>
2016-05-04 21:19:22 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'path.py>=8.1.2',
         'nose',
         'retrying==1.3.3',
-        'cloudify==3.4a4',
+        'cloudify==3.4a5',
         'elasticsearch',
         'Jinja2==2.7.2',
         'influxdb==0.1.13',
