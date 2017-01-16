1f4aaf32b53e18aa4aa0338c9977461ec8755abe
haviv <Havivv1305@gmail.com>
2017-01-16 09:57:29 +0200

    fix remove tmp

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,9 +8,5 @@ PYTHON_FILE_SERVER_ROOT=${TEMP_DIR}/python-simple-http-webserver
 ctx logger info "Shutting down 'python -m SimpleHTTPServer' process"
 pkill -9 -f 'python -m SimpleHTTPServer'
 
-ctx logger info "Deleting all files from hte server "
-
-pushd /tmp
-    shopt -s extglob
-    rm -rf  -- !(task-*|*.socket)
-popd
+ctx logger info "Deleting file server root directory (${PYTHON_FILE_SERVER_ROOT})"
+rm -rf ${PYTHON_FILE_SERVER_ROOT}
