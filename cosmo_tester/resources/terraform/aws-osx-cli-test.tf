16d094cd299400d345079ee9d44ebf8b1ad6c600
Assi Maimon <assi@gigaspaces.com>
2017-09-25 15:06:58 +0300

    remove key after build (#584)
    another percussion in case key exsists

--- cosmo_tester/resources/terraform/aws-osx-cli-test.tf
+++ cosmo_tester/resources/terraform/aws-osx-cli-test.tf
@@ -99,6 +99,7 @@ resource "null_resource" "macincloud" {
       "chmod +x /tmp/osx-cli-test.sh",
       "export MACINCLOUD_PASSWORD=${var.osx_password}",
       "/tmp/osx-cli-test.sh ${var.cli_package_url} /tmp/key.pem ${aws_instance.manager.public_ip} ${aws_instance.manager.private_ip} ${var.manager_user}"
+      "rm -rf /tmp/key.pem"
     ]
   }
 }
