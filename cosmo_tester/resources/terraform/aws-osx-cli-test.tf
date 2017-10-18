f70c51f33c250d3dc5d1a8bf968a25e6b86dc4c7
Assi Maimon <assi@gigaspaces.com>
2017-10-18 17:10:16 +0300

    fix send command to manager instance

--- cosmo_tester/resources/terraform/aws-osx-cli-test.tf
+++ cosmo_tester/resources/terraform/aws-osx-cli-test.tf
@@ -68,13 +68,6 @@ resource "aws_instance" "manager" {
   tags {
     "Name" = "system-test OSX cli-test"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo hello world",
-      "sudo yum update openssl -y"
-    ]
-  }
 }
 
 # macincoud Dedicated server
@@ -107,6 +100,7 @@ resource "null_resource" "macincloud" {
     inline = [
       "chmod +x /tmp/osx-cli-test.sh",
       "export MACINCLOUD_PASSWORD=${var.osx_password}",
+      "ssh -i /tmp/key.pem centos@${aws_instance.manager.public_ip} 'sudo yum update openssl -y'",
       "/tmp/osx-cli-test.sh ${var.cli_package_url} /tmp/key.pem ${aws_instance.manager.public_ip} ${aws_instance.manager.private_ip} ${var.manager_user}",
       "rm -rf /tmp/key.pem"
     ]
