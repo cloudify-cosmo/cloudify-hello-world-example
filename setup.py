0c8bb4af403573fcf1ccdbc07a60f1c94fa383c1
idob-cloudify <33690603+idob-cloudify@users.noreply.github.com>
2018-03-26 09:56:19 +0300

    update wagon to 0.6.2 (#653)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'fasteners==0.13.0',
         # Wagon version has been left out since it better reflects the user
         # use-case
-        'wagon[venv]==0.6.1',
+        'wagon[venv]==0.6.2',
         'pytest==3.0.4',
         'testtools==2.2.0',
         'openstacksdk==0.9.13'
