0dd4f6a29984a851cf3315e5d92f16737a145eda
idob-cloudify <33690603+idob-cloudify@users.noreply.github.com>
2018-05-31 15:28:56 +0300

    run stage system tests (#694)
    * run stage system tests
    
    * Update ui_test.py
    
    * Update ui_test.py

--- cosmo_tester/test_suites/ui_tests/ui_test.py
+++ cosmo_tester/test_suites/ui_tests/ui_test.py
@@ -17,52 +17,13 @@
 
 from cosmo_tester.framework.fixtures import image_based_manager
 
+import subprocess
+import os
+
 manager = image_based_manager
-STAGE_E2E_SELENIUM_HOST = '10.239.0.203'
 
 
 def test_ui(cfy, manager, module_tmpdir, attributes, ssh_key, logger):
 
-    # Example of using fabric_ssh
-
-    # with manager.ssh() as fabric_ssh:
-    #     logger.info('Validating old `certificate_metadata`...')
-    #     fabric_ssh.get(
-    #         remote_metadata_path, local_old_metadata_path, use_sudo=True
-    #     )
-    #     with open(local_old_metadata_path, 'r') as f:
-    #         old_metadata = yaml.load(f)
-    #
-    #     assert old_metadata['networks'] == old_networks
-    #
-    #     logger.info('Putting the new `certificate_metadata`...')
-    #     fabric_ssh.put(
-    #         local_metadata_path, remote_metadata_path, use_sudo=True
-    #     )
-    #
-    #     ip_setter_path = '/opt/cloudify/manager-ip-setter/'
-    #     restservice_python = '/opt/manager/env/bin/python'
-    #     update_ctx_script = join(ip_setter_path,
-    #                       'update-provider-context.py')
-    #
-    #     logger.info('Updating the provider context...')
-    #     fabric_ssh.sudo('{python} {script} --networks {networks} {ip}'.
-    #         format(
-    #         python=restservice_python,
-    #         script=update_ctx_script,
-    #         networks=remote_metadata_path,
-    #         ip=private_ip
-    #     ))
-    #
-    #     logger.info('Recreating internal certs')
-    #     fabric_ssh.sudo(
-    #         '{cfy_manager} create-internal-certs '
-    #         '--metadata {metadata} '
-    #         '--manager-ip {ip}'
-    #             .format(
-    #             cfy_manager='/usr/bin/cfy_manager',
-    #             metadata=remote_metadata_path,
-    #             ip=private_ip
-    #         )
-    #     )
-    pass
+    os.environ["STAGE_E2E_MANAGER_URL"] = manager.ip_address
+    subprocess.call(['npm', 'run', 'e2e'])
