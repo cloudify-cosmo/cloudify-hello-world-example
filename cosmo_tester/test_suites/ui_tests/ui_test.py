84e0c77997b6120c7e55862d01b285395f94897f
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2018-06-06 13:44:27 +0200

    Added Stage package upload to Manager (#698)

--- cosmo_tester/test_suites/ui_tests/ui_test.py
+++ cosmo_tester/test_suites/ui_tests/ui_test.py
@@ -25,6 +25,26 @@ manager = image_based_manager
 
 def test_ui(cfy, manager, module_tmpdir, attributes, ssh_key, logger):
 
+    if os.environ["UPDATE_STAGE_ON_MANAGER"] == 'true':
+        logger.info('Starting update of Stage package on Manager...')
+        if os.environ["CENTOS_MANAGER"] == 'false':
+            logger.info('Setting Manager user to cloud-user')
+            os.environ["MANAGER_USER"] = 'cloud-user'
+        os.environ["MANAGER_IP"] = manager.ip_address
+        os.environ["SSH_KEY_PATH"] = ssh_key.private_key_path
+        if not os.environ["STAGE_PACKAGE_URL"]:
+            logger.info('Creating Stage package...')
+            subprocess.call(['npm', 'run', 'beforebuild'],
+                            cwd=os.environ["CLOUDIFY_STAGE_REPO_PATH"])
+            subprocess.call(['npm', 'run', 'build'],
+                            cwd=os.environ["CLOUDIFY_STAGE_REPO_PATH"])
+            subprocess.call(['npm', 'run', 'zip'],
+                            cwd=os.environ["CLOUDIFY_STAGE_REPO_PATH"])
+        logger.info('Uploading Stage package...')
+        subprocess.call(['npm', 'run', 'upload'],
+                        cwd=os.environ["CLOUDIFY_STAGE_REPO_PATH"])
+
+    logger.info('Starting Stage system tests...')
     os.environ["STAGE_E2E_SELENIUM_HOST"] = '10.239.0.203'
     os.environ["STAGE_E2E_MANAGER_URL"] = manager.ip_address
     subprocess.call(['npm', 'run', 'e2e'],
