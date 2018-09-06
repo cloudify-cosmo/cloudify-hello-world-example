aa08ad74160bd55972c0adb2b92c1730cfb27be3
opencm <limor@gigaspaces.com>
2018-09-06 12:30:57 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-system-tests',
-    version='4.5.dev1',
+    version='4.5',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=['cosmo_tester'],
